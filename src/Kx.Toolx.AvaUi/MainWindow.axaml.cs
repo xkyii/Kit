08aa7ece48a2c9096ec3c3a2cee900e55e55dd8b
xkyii <xkyii@foxmail.com>
2024-01-23 23:09:19 +0800

    Up: AppWindow

--- src/Kx.Toolx.AvaUi/MainWindow.axaml.cs
+++ src/Kx.Toolx.AvaUi/MainWindow.axaml.cs
@@ -1,11 +1,13 @@
-using Avalonia.Controls;
+using FluentAvalonia.UI.Windowing;
 
 namespace Kx.Toolx.AvaUi;
 
-public partial class MainWindow : Window
+public partial class MainWindow : AppWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+        Title = "ffff";
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
     }
 }
