0a0507aabd366fc6e7ab8ed2e9896d92200cb026
xkyii@th <xkyii@foxmail.com>
2024-12-05 10:57:08 +0800

    Up: tab -> space

--- src/Kx.Toolx.Clicky/HostedService.cs
+++ src/Kx.Toolx.Clicky/HostedService.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Windows.Win32.Foundation;
-
 using static Windows.Win32.PInvoke;
 
 
@@ -11,85 +10,85 @@ namespace Kx.Toolx.Clicky;
 
 internal class HostedService : BackgroundService
 {
-	private readonly ILogger _logger;
-	private readonly ClickyConfig _config;
-	private Timer? _timer;
+    private readonly ILogger _logger;
+    private readonly ClickyConfig _config;
+    private Timer? _timer;
 
-	public HostedService(ILogger<HostedService> logger, IOptions<ClickyConfig> options)
-	{
-		_logger = logger;
-		_config = options.Value;
+    public HostedService(ILogger<HostedService> logger, IOptions<ClickyConfig> options)
+    {
+        _logger = logger;
+        _config = options.Value;
 
-		_logger.LogInformation(_config.ToString());
-	}
+        _logger.LogInformation(_config.ToString());
+    }
 
-	protected override Task ExecuteAsync(CancellationToken stoppingToken)
-	{
-		_timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(_config.TimerPeriod));
-		return Task.CompletedTask;
-	}
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(_config.TimerPeriod));
+        return Task.CompletedTask;
+    }
 
-	private async void DoWork(object? state)
-	{
-		var point = RandomPoint();
-		var hwnd = GetConfigHwnd();
+    private async void DoWork(object? state)
+    {
+        var point = RandomPoint();
+        var hwnd = GetConfigHwnd();
 
-		_logger.LogInformation($"点击 - 0x{hwnd.Value.ToString("X8")} - ({point.X}, {point.Y})");
+        _logger.LogInformation($"点击 - 0x{hwnd.Value.ToString("X8")} - ({point.X}, {point.Y})");
 
-		var pos = (LPARAM)(nint)(uint)(point.X | point.Y << 16);
-		SendMessage(hwnd, WM_LBUTTONDOWN, (WPARAM)0, pos);
-		await Task.Delay(50);
-		SendMessage(hwnd, WM_LBUTTONUP, (WPARAM)0, pos);
-	}
+        var pos = (LPARAM)(nint)(uint)(point.X | point.Y << 16);
+        SendMessage(hwnd, WM_LBUTTONDOWN, (WPARAM)0, pos);
+        await Task.Delay(50);
+        SendMessage(hwnd, WM_LBUTTONUP, (WPARAM)0, pos);
+    }
 
-	private Point RandomPoint()
-	{
-		if (_config.Points == null || _config.Points.Count() == 0)
-		{
-			return Point.Empty;
-		}
+    private Point RandomPoint()
+    {
+        if (_config.Points == null || _config.Points.Count() == 0)
+        {
+            return Point.Empty;
+        }
 
-		var x = (new Random().Next() % _config.Points.Count());
-		//_logger.LogInformation($"x: {x}");
-		//foreach (var p in _config.Points)
-		//{
-		//	_logger.LogInformation($"X: {p.X}, Y: {p.Y}");
-		//}
-		return _config.Points[x];
-	}
+        var x = (new Random().Next() % _config.Points.Count());
+        //_logger.LogInformation($"x: {x}");
+        //foreach (var p in _config.Points)
+        //{
+        //	_logger.LogInformation($"X: {p.X}, Y: {p.Y}");
+        //}
+        return _config.Points[x];
+    }
 
-	private HWND GetConfigHwnd()
-	{
-		if (_config == null || string.IsNullOrEmpty(_config.Hwnd))
-		{
-			return HWND.Null;
-		}
+    private HWND GetConfigHwnd()
+    {
+        if (_config == null || string.IsNullOrEmpty(_config.Hwnd))
+        {
+            return HWND.Null;
+        }
 
 
-		// 十六进制
-		if (_config.Hwnd.StartsWith("0x", StringComparison.CurrentCultureIgnoreCase))
-		{
-			if (long.TryParse(_config.Hwnd.Substring(2), System.Globalization.NumberStyles.HexNumber, null, out var h))
-			{
-				return (HWND)h;
-			}
-		}
+        // 十六进制
+        if (_config.Hwnd.StartsWith("0x", StringComparison.CurrentCultureIgnoreCase))
+        {
+            if (long.TryParse(_config.Hwnd.Substring(2), System.Globalization.NumberStyles.HexNumber, null, out var h))
+            {
+                return (HWND)h;
+            }
+        }
 
-		// 十进制
-		else 
-		{
-			if (long.TryParse(_config.Hwnd, out var h))
-			{
-				return (HWND)h;
-			}
-		}
+        // 十进制
+        else
+        {
+            if (long.TryParse(_config.Hwnd, out var h))
+            {
+                return (HWND)h;
+            }
+        }
 
-		return HWND.Null;
-	}
+        return HWND.Null;
+    }
 
-	public override void Dispose()
-	{
-		base.Dispose();
-		_timer?.Dispose();
-	}
+    public override void Dispose()
+    {
+        base.Dispose();
+        _timer?.Dispose();
+    }
 }
--- src/Kx.Toolx.Clicky/Program.cs
+++ src/Kx.Toolx.Clicky/Program.cs
@@ -13,78 +13,81 @@ Console.WriteLine($"App Folder: {folder}");
 
 Log.Logger = new LoggerConfiguration()
 #if DEBUG
-	.MinimumLevel.Debug()
+    .MinimumLevel.Debug()
 #else
-	.MinimumLevel.Information()
+    .MinimumLevel.Information()
 #endif
-	.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-	.Enrich.FromLogContext()
-	.WriteTo.Console()
-	.CreateLogger();
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
 
 Log.Information("Starting console host.");
 
 
 try
 {
-	await Host.CreateDefaultBuilder(args)
-		.ConfigureAppConfiguration((context, builder) =>
-		{
-			var env = context.HostingEnvironment;
-			var contentRootPath = context.HostingEnvironment.ContentRootPath;
+    await Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, builder) =>
+            {
+                var env = context.HostingEnvironment;
+                var contentRootPath = context.HostingEnvironment.ContentRootPath;
 
-			Log.Information(" EnvironmentName: {env}", env.EnvironmentName);
+                Log.Information(" EnvironmentName: {env}", env.EnvironmentName);
 
-			builder.SetBasePath(folder);
-			builder.AddJsonFile("appsettings.json", true);
-			builder.AddJsonFile(Path.Combine(contentRootPath, "appsettings.json"), true);
-			builder.AddJsonFile(Path.Combine(contentRootPath, $"appsettings.{env.EnvironmentName}.json"), true);
-		})
-		.ConfigureLogging(builder =>
-		{
-			builder.ClearProviders();
-			builder.AddSerilog();
-		})
-		.ConfigureServices((hostContext, services) =>
-		{
-			services.AddHostedService<HostedService>();
+                builder.SetBasePath(folder);
+                builder.AddJsonFile("appsettings.json", true);
+                builder.AddJsonFile(Path.Combine(contentRootPath, "appsettings.json"), true);
+                builder.AddJsonFile(Path.Combine(contentRootPath, $"appsettings.{env.EnvironmentName}.json"), true);
+            })
+            .ConfigureLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.AddSerilog();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<HostedService>();
 
-			// Options
-			// 原配置
-			// services.Configure<ClickyConfig>(hostContext.Configuration.GetSection(ClickyConfig.KEY));
+                // Options
+                // 原配置
+                // services.Configure<ClickyConfig>(hostContext.Configuration.GetSection(ClickyConfig.KEY));
 
-			// 替换后
-			services.AddOptions();
-			var config = hostContext.Configuration.GetSection(ClickyConfig.KEY);
-			services.AddSingleton<IOptionsChangeTokenSource<ClickyConfig>>(new ConfigurationChangeTokenSource<ClickyConfig>(string.Empty, config));
-			services.AddSingleton<IConfigureOptions<ClickyConfig>>(new ClickyConfigureNamedOptions(string.Empty, config));
-		})
-		.UseConsoleLifetime()
-		.Build()
-		.RunAsync()
-		;
-
+                // 替换后
+                services.AddOptions();
+                var config = hostContext.Configuration.GetSection(ClickyConfig.KEY);
+                services.AddSingleton<IOptionsChangeTokenSource<ClickyConfig>>(
+                    new ConfigurationChangeTokenSource<ClickyConfig>(string.Empty, config));
+                services.AddSingleton<IConfigureOptions<ClickyConfig>>(
+                    new ClickyConfigureNamedOptions(string.Empty, config));
+            })
+            .UseConsoleLifetime()
+            .Build()
+            .RunAsync()
+        ;
 }
 catch (Exception ex)
 {
-	Log.Error(ex.ToString());
+    Log.Error(ex.ToString());
 }
 finally
 {
-	Log.CloseAndFlush();
+    Log.CloseAndFlush();
 }
 
+return;
 
 
 static string genSpecialFolder()
 {
-	// 获取当前用户的用户文件夹路径
-	string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-	string specificPath = Path.Combine(path, ".xky", "clicky");
-	// 如果子文件夹不存在，则创建它
-	if (!Directory.Exists(specificPath))
-	{
-		Directory.CreateDirectory(specificPath);
-	}
-	return specificPath;
+    // 获取当前用户的用户文件夹路径
+    var path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+    var specificPath = Path.Combine(path, ".xky", "clicky");
+    // 如果子文件夹不存在，则创建它
+    if (!Directory.Exists(specificPath))
+    {
+        Directory.CreateDirectory(specificPath);
+    }
+
+    return specificPath;
 }
